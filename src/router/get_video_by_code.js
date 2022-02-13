b008aaf0c59da55164044ddcebf179ca88c3cdab
manhblue <phamducmanh1452001@gmail.com>
2022-02-13 22:41:05 +0700

    opt

--- src/router/get_video_by_code.js
+++ src/router/get_video_by_code.js
@@ -30,71 +30,13 @@ router.get('/:code', function(req, res, next) {
             res.status(404).json({
                 'message': 'not found',
             });
-        } else if (files != undefined) {
-            video.files = files;
-            res.json(
-                new VideoResponse(video, true)
-            );
-        }
-    });
-    
-    getFileFromCode(code, clientIp)
-    .then(values => {
-        if (video != undefined) {
-            video.files = values;
-
-            cachedVideosByCode.set(code, video);
-            setTimeout(() => {
-                cachedVideosByCode.delete(code);
-            }, 1500 * 3600);
-
-            res.json(
-                new VideoResponse(video, true)
-            );
         } else {
-            files = values;
+            video.files = {};
+            res.json(
+                new VideoResponse(video, true)
+            );
         }
     });
-});
-
-const getFileFromCode = async (code, clientIp) => new Promise((resolve) => {
-    const options = { localAddress: clientIp }
-    const httpAgent = new http.Agent(options);
-    const httpsAgent = new https.Agent(options);
-    const uri = `https://smartshare.tv/api/source/${code}`;
-    let items = [];
-    axios
-        .post(uri, { httpAgent, httpsAgent })
-        .then(_res => {
-            items = _res.data.data;
-        })
-        .catch(err => {
-            resolve(undefined);
-        })
-        .then(() => {
-            if (items == undefined) {
-                console.log('files are undefined');
-                return;
-            }
-            let files = {
-                '1080p': '',
-                '720p': '',
-                '480p': '', 
-            };
-
-            for(let i = 0; i < items.length; i++) {
-                const item = items[i];
-                if (item.label == '1080p') {
-                    files['1080p'] = item.file;
-                } else if (item.label == '720p') {
-                    files['720p'] = item.file;
-                } else {
-                    files['480p'] = item.file;
-                }
-            }
-        
-            resolve(files);
-        });
 });
 
 module.exports = router;
