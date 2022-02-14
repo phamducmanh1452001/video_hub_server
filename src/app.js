66ae3184fbb13fd604aee37813e84eda3c3c4ab2
manhblue <phamducmanh1452001@gmail.com>
2022-02-14 22:54:11 +0700

    add ssl

--- src/app.js
+++ src/app.js
@@ -2,12 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors')
 const app = express();
+const https = require('https');
+const fs = require('fs');
+const privateKey  = fs.readFileSync('src/ujav_xyz_privkey.key', 'utf8');
+const certificate = fs.readFileSync('src/ujav_xyz_certificate.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
 
 // debug middle
-// app.use('/api', (req, res, next) => {
-//   console.log(req.path);
-//   next();
-// });
+app.use('/api', (req, res, next) => {
+  console.log(req.socket.remoteAddress);
+  next();
+});
+
 // default middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -20,7 +26,8 @@ app.get('*', function(req, res) {
   res.sendFile('index.html', {root: './public'});
 });
 
-const server = app.listen( process.env.PORT || 80, () => {
+const server = https.createServer(credentials, app);
+server.listen(443, () => {
     console.log('Listening on port ' + server.address().port);
 });
 // mongoimport --db ujav --collection videos --file videos.json
